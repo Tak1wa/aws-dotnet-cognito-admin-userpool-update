94e75b50aaee8a4ffe209f2c810857d7a24a417d
Tak1wa <iwasa.takahito@classmethod.jp>
2022-07-30 07:42:48 +0900

    hoge

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+
+var client = new AmazonCognitoIdentityProviderClient();
+var result = await client.AdminUpdateUserAttributesAsync(new AdminUpdateUserAttributesRequest
+{
+    UserPoolId = "ap-northeast-1_zVyUOeuZo",
+    Username = "hoge-user1",
+    UserAttributes = new List<AttributeType>
+    {
+        new AttributeType { Name = "name", Value = "user111"},
+        new AttributeType { Name = "nickname", Value = "hogehogehoge"}
+    }
+});
+
+Console.WriteLine(result.HttpStatusCode);
